818fa9b9823913fbf6cb764f7a6be23e0a78a71d
Rubén López Gea <contactar@gmail.com>
2024-09-01 20:08:12 +0200

    Add spanish translations

---
+++ resources/lang/es/package.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'permalink_label' => 'Enlace:',
+    'permalink_label_link_visit' => 'Visitar',
+    'permalink_action_edit' => 'Editar',
+    'permalink_action_ok' => 'OK',
+    'permalink_action_reset' => 'Reset',
+    'permalink_action_cancel' => 'Cancelar',
+    'permalink_status_changed' => 'cambiado',
+];
